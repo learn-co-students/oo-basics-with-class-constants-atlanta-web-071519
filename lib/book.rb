b81b95f811584bbc2cee41714fc34e3d595f23e1
github username <github email address>
2019-07-20 17:34:11 -0400

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,5 +1,9 @@
 class Book
-  attr_accessor :author, :page_count, :genre
+  
+ 
+  GENRES = []
+  
+  attr_accessor :author, :page_count
   attr_reader :title
 
   def initialize(title)
@@ -9,5 +13,10 @@ class Book
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
+
+  def genre=(genre)
+    @genre = genre
+    GENRES << @genre
+  end 
 
 end
